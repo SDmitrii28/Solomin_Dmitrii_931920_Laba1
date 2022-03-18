353e120b9f74e8480fc1f2950702542fae282858
Dmitrii <ssdmitrii28@gmail.com>
2022-03-18 15:32:17 +0700

    Initializing methods in the PersonKeeper class

--- person_keeper.h
+++ person_keeper.h
@@ -4,22 +4,74 @@
 #include <QFile>
 #include <QString>
 #include "Stack.h"
+#include "person.h"
 
 //Singleton
 class PersonKeeper
 {
 private:
-    PersonKeeper(); // конструкторы по умолчанию
+    PersonKeeper(){} // конструкторы по умолчанию
     PersonKeeper(const PersonKeeper &); //конструктор копирования
-    ~PersonKeeper();//деструктор
+    ~PersonKeeper(){}//деструктор
     PersonKeeper &operator=(const PersonKeeper &); //оператор присовения
+    Stack<Person> st; // имена
 public:
     static PersonKeeper &Instance(); // хранится единственный экземпляр класса
-    void ReadPersons(QString path); //записать ФИО в файл
-    void WritePersons(QString path) const; //считать ФИО из файла
+    void ReadPersons(QString path); //считать ФИО из файла
+    void WritePersons(QString path) const; //записать ФИО в файл
     QStringList ToQStringList() const; //экспорт стэка в список строк
     int Size(); // количество ФИО
     void Clear(); // очистить список ФИО
 
 };
+//Размер
+int PersonKeeper::Size()
+{
+    return st.Size();
+}
+//Удаление
+void PersonKeeper::Clear()
+{
+    return st.Clear();
+}
+// хранится единственный экземпляр класса
+PersonKeeper &PersonKeeper::Instance()
+{
+    static PersonKeeper inst;
+    return inst;
+}
+
+//считать ФИО из файла
+void PersonKeeper::ReadPersons(QString path)
+{
+    QFile file(path);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) // открытие файла на чтение
+    {
+        throw "Not open file"; // файл не открылся
+    }
+    QTextStream stream(&file); // поток текстовых данных файла
+    QString line; // в буфер считываются строки
+
+    while (stream.readLineInto(&line)) // считытваем, пока не конец файла
+    {
+        st.Push(Person(line)); // добавляем в стек ФИО
+    }
+    file.close(); // закрываем данный файл
+}
+
+//записать ФИО в файл
+void PersonKeeper::WritePersons(QString path) const
+{
+    QFile file(path);
+    if (!file.open(QIODevice::WriteOnly | QIODevice::Text)) // открытие файла на запись
+    {
+        throw "Not open file"; // файл не открылся
+    }
+    QTextStream stream(&file); // поток текстовых данных файла
+    st.Enum([&](const Person &k) // перебираем значения
+    {
+        stream << k.Get_last_name() << '\t' << k.Get_first_name() << '\t' << k.Get_patronymic() << Qt::endl; // записываем ФИО в файл
+    });
+    file.close(); // закрываем файл
+}
 #endif // PERSON_KEEPER_H
